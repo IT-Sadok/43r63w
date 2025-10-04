fe8d5ff019af10c900111ca496ce5de58568088a
VLADYSLAV TKACHENKO <vt7098386@gmail.com>
2025-10-04 16:39:19 +0300

    Made first homework

---
+++ TaskManager/TaskManager/Book.cs
@@ -0,0 +1,19 @@
+﻿public class Book
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public string Author { get; set; } = null!;
+
+    public decimal Price { get; set; }
+
+    public double Rating { get; set; }
+
+    public async Task UpdateTitleAsync(string newTitle, Book book)
+    {
+
+    }
+}
---
+++ TaskManager/TaskManager/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Data;
+using System.Threading.Tasks;
+
+namespace TaskManager
+{
+    internal class Program
+    {
+        private static readonly object _lock = new object();
+
+        private static readonly Book _book = new Book
+        {
+            Id = 1,
+            Title = "Sample Book",
+            Description = "This is a sample book description.",
+            Author = "John Doe",
+            Price = 19.99m,
+            Rating = 4.5
+        };
+
+
+        static async Task Main(string[] args)
+        {
+            var tasks = new Task[100];
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                var taskNumber = i;
+                tasks[i] = Task.Run(() => UpdateBookTitle(taskNumber.ToString()));
+            }
+
+            await Task.WhenAll(tasks);
+
+            Console.WriteLine($"Final Book Title: {_book.Title}");
+        }
+
+        static void UpdateBookTitle(string titleName)
+        {
+            lock (_lock)
+            {
+                _book.Title = $"Sample Book {titleName}";
+            }
+        }
+
+    }
+}
